14dacae69b0b8c82faebc07072f28ee812f2b3fd
元凛 <xrkffgg@gmail.com>
2021-05-17 23:25:12 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,38 @@
-import Temp, { TempProps } from './template';
+const SymbolRegex = /[.,\/#!$%\^&\*;:{}=\-_`~()]/g;
 
-export { TempProps };
-export default Temp;
+export type StringOrStrings = string | string[];
+
+/**
+ *
+ * @param s The string or strings to be replaced
+ * @param r The content to be replaced
+ * @param reg Customize RegExp
+ * @returns Replaced result
+ */
+const replaceSymbol = (
+  s: StringOrStrings,
+  r: string = '',
+  reg: RegExp = SymbolRegex,
+) => {
+  if (typeof s === 'string') {
+    return s.replace(reg, r);
+  }
+
+  if (typeof s === 'object') {
+    const a: string[] = [];
+    s.forEach((i) => {
+      if (typeof i === 'string') {
+        a.push(i.replace(reg, r));
+      } else {
+        a.push(i);
+      }
+    });
+    return a;
+  }
+
+  return '';
+};
+
+export { replaceSymbol };
+
+export default replaceSymbol;
